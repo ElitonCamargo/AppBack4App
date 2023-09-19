88f93735ca6420eac9fbfc65896e50caec6cbb19
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2023-09-19 08:52:55 -0300

    Ambiente preparado

---
+++ App.jsx
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import Cadastrar from './components/cadastrar';
+import Consultar from './components/consultar';
+
+const Tabs = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tabs.Navigator>        
+        <Tabs.Screen name='Cadastrar' component={Cadastrar}
+          options={{
+            tabBarLabel:'Home',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="add-circle-outline" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen name='Consultar' component={Consultar} 
+          options={{
+            tabBarLabel: 'Listar',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="list-circle-outline" size={size} color={color} />
+            ),            
+          }}
+        />
+      </Tabs.Navigator>
+    </NavigationContainer>    
+  );
+}
